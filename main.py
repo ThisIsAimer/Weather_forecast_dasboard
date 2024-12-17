0447b047a5c942ac236b95682a26180499e957b9
raunak <raunakbiswas05@gmail.com>
2024-12-17 18:06:24 +0530

    Added a graph in the web app with plotly.express

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as web
+import plotly.express as plotly
 
 web.title("Weather Forecast Dashboard")
 place = web.text_input("place:",placeholder="Ex: Japan")
@@ -6,3 +7,11 @@ days = web.slider("Days:", 1, 5, help="Select the number of days you want the fo
 selection = web.selectbox("Type of data you want to view:",("Temperature","Weather"))
 
 web.header(f"{selection} for the next {days} days in {place}:")
+
+dates = ["oct 1st","oct 5th", "oct 10th", "oct 15th", "oct 20"]
+temperature = [20,19,22,19,16]
+
+
+
+graph = plotly.line(x= dates, y= temperature, labels={"x":"Dates","y":"Temperature (C)"})
+web.plotly_chart(graph)
